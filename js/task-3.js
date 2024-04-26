f44ccb497fb371a61be70c2682d136654246ce4d
Nikitin <161200278+Nikitincv@users.noreply.github.com>
2024-04-26 11:41:22 +0200

    1

--- js/task-3.js
+++ js/task-3.js
@@ -6,4 +6,6 @@ function getElementWidth(content, padding, border) {
   );
 }
 
-
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20)); 
+console.log(getShippingMessage("Sweden", 100, 20));
